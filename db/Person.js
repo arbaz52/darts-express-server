5087c376d1accda50a3ca50a6e316369747f2b25
arbaz52 <arbaz5256@gmail.com>
2020-07-12 00:08:35 +0500

    testing custom validator on person.fullName

--- db/Person.js
+++ db/Person.js
@@ -1,10 +1,18 @@
 var mongoose = require('./connection')
 var Schema = mongoose.Schema
 
+let validateLength = (v) => {
+    return v.length > 2;
+}
+
 var Person = new Schema({
     fullName: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        validate: [
+            validateLength, "Invalid fullname, should be more than 2 characters long"
+        ]
     },
     gender: {
         type: String,
